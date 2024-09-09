dffe1ad89b14aaabf8b80eb8139be6252f068b81
jihed <jihedoueslati4@gmail.com>
2024-09-09 20:41:22 +0200

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ProfilePage from './components/ProfilePage'; // Ensure this path is correct
+import ProfilePage from './components/ProfilePage'; // Make sure the path to ProfilePage is correct
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Remove or update the following line if HomePage is not present */}
-        {/* <Route path="/" element={<HomePage />} /> */}
-        <Route path="/profile" element={<ProfilePage />} />
+        {/* Set the ProfilePage as the default route at the root "/" */}
+        <Route path="/" element={<ProfilePage />} />
       </Routes>
     </Router>
   );
